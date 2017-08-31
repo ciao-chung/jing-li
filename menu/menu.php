7170077627d49a904e3c91929e636ccc8db780da
Ciao <ciao0958@gmail.com>
2017-08-31 17:22:46 +0800

    dev

---
+++ menu/menu.php
@@ -0,0 +1,9 @@
+<div data-role="nav-wrap">
+    <div data-role="nav-brand-wrap"></div>
+
+    <ul data-role="nav-list">
+        <li data-role="nav-item">
+            <a></a>
+        </li>
+    </ul>
+</div>
--- public/index.php
+++ public/index.php
@@ -12,17 +12,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <meta charset="utf-8">
         <meta name="keywords" content="">
-        <meta name="description" content="">
+        <meta name="description" content="<?php echo $GLOBALS['page_title']; ?>">
         <meta property="og:title" content="<?php echo $GLOBALS['page_title']; ?>">
         <meta property="og:site_name" content="<?php echo $config->company->name; ?>">
         <meta property="og:url" content="<?php echo $GLOBALS['domain']; ?>">
         <meta property="og:image" content="">
         <script src="/node_modules/jquery/dist/jquery.min.js"></script>
         <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="dist/css/main.css">
     </head>
     <body>
-        <?php
-            include_once $view_path.$page.'.php';
-        ?>
+        <?php include_once ''; ?>
+        <?php include_once $view_path.$page.'.php'; ?>
     </body>
 </html>
