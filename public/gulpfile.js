951a87c77b0d64158070cdc1ac0e3fb91d6361af
Ciao <ciao0958@gmail.com>
2017-08-31 11:57:26 +0800

    調整gulp

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,11 +1,15 @@
 // gulp [task name], example: gulp sass
-
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulpCompass = require('gulp-compass');
 
 // compiler sass
 gulp.task('sass',function(){
-  return gulp.src('sass/*.sass')
-    .pipe(sass().on('error',sass.logError))
+  return gulp.src('sass/*.sass') // sass 來源路徑
+    .pipe(gulpCompass({
+      css: 'dist',           // compass 輸出位置
+      sass: 'sass',      // sass 來源路徑
+      image: 'sass',   // 圖片來源路徑
+      style: 'compressed',                // CSS 處理方式，預設 nested（expanded, nested, compact, compressed）
+    }))
     .pipe(gulp.dest('dist/css'));
 });
