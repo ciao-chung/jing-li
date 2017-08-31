8f7e73a3a464c63f4b991cf7e875c01be1342167
Ciao <ciao0958@gmail.com>
2017-08-31 11:22:18 +0800

    環境檔改為json

--- public/index.php
+++ public/index.php
@@ -1,6 +1,13 @@
+<?php
+    $config = json_decode(file_get_contents(__DIR__.'/../data/config.json'));
+?>
 <html>
     <head>
-        <title>test</title>
+        <title>
+            <?php echo $config->company->name; ?>
+        </title>
+        <script src="/node_modules/jquery/dist/jquery.min.js"></script>
+        <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
     </head>
     <body>
         <?php include_once ('route.php'); ?>
