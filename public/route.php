13194d9bbb9b4e478dd19fa4b809ff12ad1d0e94
Ciao <ciao0958@gmail.com>
2017-08-31 10:57:41 +0800

    加入聯絡我們, 水管清洗頁

--- public/route.php
+++ public/route.php
@@ -9,6 +9,14 @@
             include_once $view_path.'root.php';
             break;
 
+        case 'tube-cleaning':
+            include_once $view_path.'tube-cleaning.php';
+            break;
+
+        case 'contactus':
+            include_once $view_path.'contactus.php';
+            break;
+
         // 文案
         case 'post':
             $code = $_GET['code'];
