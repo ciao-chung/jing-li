1dbfd97bb1f048a22881f0b6fb84c4445f9ceefd
Ciao <ciao0958@gmail.com>
2017-08-31 12:02:10 +0800

    gulp加入watch

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -2,14 +2,19 @@
 var gulp = require('gulp');
 var gulpCompass = require('gulp-compass');
 
+// watch sass
+gulp.task('watch', function () {
+  gulp.watch('sass/*.sass', ['sass']);
+});
+
 // compiler sass
 gulp.task('sass',function(){
+  console.log('task sass')
   return gulp.src('sass/*.sass') // sass 來源路徑
     .pipe(gulpCompass({
-      css: 'dist',           // compass 輸出位置
+      css: 'dist/css',           // compass 輸出位置
       sass: 'sass',      // sass 來源路徑
       image: 'sass',   // 圖片來源路徑
       style: 'compressed',                // CSS 處理方式，預設 nested（expanded, nested, compact, compressed）
-    }))
-    .pipe(gulp.dest('dist/css'));
+    }));
 });
