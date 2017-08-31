0636c47961f7bcdab69ef78398a28ff9109208eb
Ciao <ciao0958@gmail.com>
2017-08-31 16:53:26 +0800

    調整page title為global

--- public/index.php
+++ public/index.php
@@ -1,19 +1,19 @@
 <?php
     $config = json_decode(file_get_contents(__DIR__.'/../data/config.json'));
 
-    $page_title = $config->company->name;
+    $GLOBALS['page_title'] = $config->company->name;
 
     include_once 'route.php';
 ?>
 <html>
     <head>
         <title>
-            <?php echo $page_title; ?>
+            <?php echo $GLOBALS['page_title']; ?>
         </title>
         <script src="/node_modules/jquery/dist/jquery.min.js"></script>
         <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
     </head>
     <body>
-        <?php include_once $view_path.$page.'.php'; ?>
+        <?php //include_once $view_path.$page.'.php'; ?>
     </body>
 </html>
--- public/route.php
+++ public/route.php
@@ -6,4 +6,10 @@
 
     if($page == 'post' && !$_GET['code']) $page = '404';
     if($page != 'root' && $page != 'post' && $page != 'tube-cleaning' && $page != 'contactus') $page = 'root';
+
+    if($page == 'post') {
+        $post_code = $_GET['code'];
+        $post = include_once $data_path.'post/'.$post_code.'.php';
+        $GLOBALS['page_title'] = $post['title'];
+    }
 ?>
