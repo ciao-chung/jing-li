f72babc3012f9af0635455e36597d2dfe2a689f4
Ciao <ciao0958@gmail.com>
2017-09-01 23:50:43 +0800

    dev

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('sass',function(){
   console.log('task sass')
   return gulp.src('sass/*.sass') // sass 來源路徑
     .pipe(gulpCompass({
-      css: 'dist/css',           // compass 輸出位置
+      css: 'assets/css',           // compass 輸出位置
       sass: 'sass',      // sass 來源路徑
       image: 'sass',   // 圖片來源路徑
       style: 'compressed',                // CSS 處理方式，預設 nested（expanded, nested, compact, compressed）
