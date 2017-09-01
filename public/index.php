279ed10f03bf3c018eb1031293055964c41bcb8b
Ciao <ciao0958@gmail.com>
2017-09-01 15:49:34 +0800

    調整架構

--- public/index.php
+++ public/index.php
@@ -4,24 +4,30 @@
     $app = new \Slim\Slim(array(
         'templates.path' => '../view'
     ));
-
-
-
-    $app->get('/*', function () use ($app){
-        $app->render(
-            'index.php', array(
-                'app' => $app,
-            )
-        );
-    });
-
-//    $app->get('/', function () use ($app){
-//        $app->render('index.html');
-//    });
-//
-//    $app->get('/post/:code', function ($code) {
-//        echo "post, " . $code;
-//    });
-
-    $app->run();
 ?>
+
+<!doctype html>
+<html lang="zh-tw">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $app->get('/', function () use ($app) {
+            $app->render('home.php');
+        });
+
+        $app->get('/post/:code', function ($code) use ($app) {
+            $app->render('post.php', [
+                'code' => $code,
+            ]);
+        });
+
+        $app->run();
+    ?>
+</body>
+</html>
