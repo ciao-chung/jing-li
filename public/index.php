f981d336b64a5fb7b40f320f94a7a39ebec9c7c6
Ciao <ciao0958@gmail.com>
2017-09-01 15:44:17 +0800

    加入route

--- public/index.php
+++ public/index.php
@@ -1,8 +1,27 @@
 <?php
     require '../vendor/autoload.php';
-    $app = new \Slim\Slim();
-    $app->get('/hello/:name', function ($name) {
-        echo "Hello, " . $name;
+
+    $app = new \Slim\Slim(array(
+        'templates.path' => '../view'
+    ));
+
+
+
+    $app->get('/*', function () use ($app){
+        $app->render(
+            'index.php', array(
+                'app' => $app,
+            )
+        );
     });
+
+//    $app->get('/', function () use ($app){
+//        $app->render('index.html');
+//    });
+//
+//    $app->get('/post/:code', function ($code) {
+//        echo "post, " . $code;
+//    });
+
     $app->run();
 ?>
