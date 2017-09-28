20ae35091199cef5ec9318423d5d5d5e23089f18
Ciao <ciao0958@gmail.com>
2017-09-28 22:40:01 +0800

    加入客戶群文案

--- public/static/data/post/customer.js
+++ public/static/data/post/customer.js
@@ -1,2 +1,69 @@
-const content = '我們的客戶'
+const content = `
+<p>我們的客戶包含了許多知名的公司</p>
+
+<p>多年的耕耘讓</p>
+
+<p>我們累積了無數客戶的肯定</p>
+
+<p>至今仍不斷的增加</p>
+
+<p>&nbsp;</p>
+
+<h3>連鎖超商</h3>
+
+<ul>
+	<li>全家便利商店</li>
+	<li>OK便利商店</li>
+	<li>萊爾富便利商店</li>
+</ul>
+
+<p>&nbsp;</p>
+
+<h3>量販業</h3>
+
+<ul>
+	<li>好事多</li>
+	<li>萬客隆</li>
+	<li>東宜流通</li>
+</ul>
+
+<p>&nbsp;</p>
+
+<h3>美髮業</h3>
+
+<ul>
+	<li>曼都髮廊</li>
+	<li>小林髮廊</li>
+	<li>普洛髮廊</li>
+</ul>
+
+<p>&nbsp;</p>
+
+<h3>餐飲業</h3>
+
+<ul>
+	<li>統一星巴克</li>
+	<li>台灣大戶屋</li>
+</ul>
+
+<p>&nbsp;</p>
+
+<h3>汽車業</h3>
+
+<ul>
+	<li>SUM優質車商聯盟</li>
+	<li>HONDA汽車</li>
+</ul>
+
+<p>&nbsp;</p>
+
+<h3>其他</h3>
+
+<ul>
+	<li>內湖三軍醫院</li>
+</ul>
+
+<p>&nbsp;</p>
+
+`
 module.exports = content
